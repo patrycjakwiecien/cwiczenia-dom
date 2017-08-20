07fbc42a064986b9ae68addab590ce0f6afdb1ef
patrycjakwiecien <patrycjaapril@gmail.com>
2017-08-20 16:09:43 +0200

    form

---
+++ js/formularz.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+    
+
+function pobierzMnie(event) {
+    event.preventDefault();
+var inputs = document.getElementsByTagName('input');
+    var i = 0;
+    while(  < inputs.length) {
+        console.log(inputs[i].value)
+        i++;
+    }
+}
+          
+          // value
---
+++ js/javas.js
@@ -0,0 +1,14 @@
+'use script';
+
+function ustawTlo(){
+    
+document.getElementsByTagName('p') [0].style.backgroundColor = 'red';  
+    
+    document.getElementsByTagName('p') [1].style.backgroundColor = 'yellow';  
+}
+
+document.getElementsByTagName('input')[0].onclick = ustawTlo;
+
+
+
+
