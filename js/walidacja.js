694ca147a60af76d31caf218dc8bd7a6214d52d0
patrycjakwiecien <patrycjaapril@gmail.com>
2017-08-26 10:15:13 +0200

    formularz

--- js/walidacja.js
+++ js/walidacja.js
@@ -1,2 +1,36 @@
-'use script';
+'use strict';
 
+var wyslijBtn = document.getElementById('wyslij');
+var wszytskieZgody = document.getElementById('wszystkie-zgody');
+var zgodamarketingowa1 = document.getElementById('zgoda-marketingowa-1');
+var zgodamarketingowa2 = document.getElementById('zgoda-marketingowa-2');
+
+function stanCheckbox() {
+
+    //this.checked - zwraca true/false.
+
+    if (this.checked) {
+        zgodaMarketingowa1.checked = this.checked;
+        zgodaMarketingowa2.checked = this.checked;
+    }
+
+
+    function waliduj() {
+        if (document.getElementById('name').value.trim() == "") {
+            alert('Wypelnij imie');
+
+        }
+
+        if (document.getElementById('email').value.trim() == "") {
+            alert('Wypelnij email');
+
+        }
+
+        if (!zgodaMarketingowa1.checked) {
+            alert('Zaznacz pierwsza zgode!');
+        }
+
+    }
+
+    wyslijBtn.addEventListener('click', waliduj);
+    wyslijBtn.addEventListener('change', stanCheckbox);
